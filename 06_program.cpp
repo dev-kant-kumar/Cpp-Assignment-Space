9d0e48912fb260697919d0eaac1930d055ec5430
dev-kant-kumar <dev.mytechhandle@gmail.com>
2024-07-24 12:04:49 +0530

    Program no 09 completed

--- 06_program.cpp
+++ 06_program.cpp
@@ -1,1 +1,38 @@
 // 6. Write a C++ program that checks whether primitive values cross the limit.
+
+#include <iostream>
+#include <climits>
+#include <cfloat>
+using namespace std;
+
+int main()
+{
+    // Limits for integral types
+    cout << "Range of integral types\n\n";
+    cout << "char min: \n" << CHAR_MIN ;
+    cout << "char max: \n" << CHAR_MAX ;
+    cout << "signed char min: \n" << SCHAR_MIN ;
+    cout << "signed char max: \n" << SCHAR_MAX ;
+    cout << "unsigned char max: \n" << UCHAR_MAX ;
+
+    cout << "short min: \n" << SHRT_MIN ;
+    cout << "short max: \n" << SHRT_MAX ;
+    cout << "unsigned short max: \n" << USHRT_MAX << endl;
+
+    cout << "int min: \n" << INT_MIN ;
+    cout << "int max: \n" << INT_MAX ;
+    cout << "unsigned int max: \n" << UINT_MAX ;
+
+    cout << "long min: " << LONG_MIN << ", max: " << LONG_MAX << endl;
+    cout << "unsigned long max: " << ULONG_MAX << endl;
+
+    cout << "long long min: " << LLONG_MIN << ", max: " << LLONG_MAX << endl;
+    cout << "unsigned long long max: " << ULLONG_MAX << endl;
+
+    // Limits for floating-point types
+    cout << "\nRange of floating-point types\n\n";
+    cout << "float min: " << FLT_MIN << ", max: " << FLT_MAX << endl;
+    cout << "double min: " << DBL_MIN << ", max: " << DBL_MAX << endl;
+    cout << "long double min: " << LDBL_MIN << ", max: " << LDBL_MAX << endl;
+    return 0;
+}
--- 09_program.cpp
+++ 09_program.cpp
@@ -4,32 +4,33 @@ using namespace std;
 
 int main()
 {
-    int num = 10, num1 = 10;
+    int num = 10, newNum = 0;
 
-    cout << "Post Increment: " << endl;
-    for (int i = 0; i < 5; i++)
+    // case of pre increment and decrement
+
+    cout << "\033[1;43m\nThis c++ program show the use of pre and post increment and decrement operation \033[0m\n\n";
+
+    cout << "Value of new Num befor the use of pre increment operation : " << newNum << endl;
+
+    cout << "Values of new Num after use of pre increment operation : " << ++newNum << endl;
+
+    cout << "Value of num before the use of pre decrement operation : " << num << endl;
+
+    cout << "Values of num after use of pre decrement operation : " << --num << endl;
+
+    // case of post increment and decrement
+    cout << "\nUse of post increment operation through for loop \n";
+    for (int i = 0; i < num; i++)
     {
-        cout << i << endl;
+        cout << "Value of i : " << i << endl;
     }
 
-    cout << "Post Decrement: " << endl;
-    while (num <= 10)
+    cout << "\nUse of post decrement operation through for loop \n";
+    while (num > 0)
     {
-        cout << num << endl;
+        cout << "Value of i :" << num << endl;
         num--;
     }
 
-    cout << "Pre Increment: " << endl;
-    for (int i = 0; i < 5; ++i)
-    {
-        cout << i << endl;
-    }
-
-    cout << "Pre Decrement: " << endl;
-    while (num1 <= 10)
-    {
-        cout << num << endl;
-        --num;
-    }
     return 0;
 }
